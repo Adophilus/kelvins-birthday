e61cf47d6c0cba09a02887bf8d610caea1966aa5
Adophilus <uchenna19of@gmail.com>
2023-06-21 20:49:19 +0100

    updated view

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,17 @@
-import { type Config } from "tailwindcss";
+import type { Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        oswald: ["Oswald", "sans-serif"],
+        bungee: ["Bungee", "cursive"],
+        robotoMono: ["Roboto Mono", "monospace"],
+        notoSans: ["Noto Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
